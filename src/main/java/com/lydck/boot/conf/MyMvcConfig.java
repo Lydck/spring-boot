b8e8e264fe8940671db09df8716592c648b3e867
Lydck <1468390049@qq.com>
2017-01-12 16:24:52 +0800

    自定义interecptor

--- src/main/java/com/lydck/boot/conf/MyMvcConfig.java
+++ src/main/java/com/lydck/boot/conf/MyMvcConfig.java
@@ -4,13 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import com.lydck.boot.intercepter.MyInterceptor;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.lydck.boot")
-public class MyMvcConfig {
+public class MyMvcConfig extends WebMvcConfigurerAdapter{
 	
 	@Bean
 	public InternalResourceViewResolver viewResolver() {
@@ -20,4 +25,20 @@ public class MyMvcConfig {
 		viewResolver.setViewClass(JstlView.class);
 		return viewResolver;
 	}
+	
+	@Bean
+	public MyInterceptor myInterecptor() {//自定义请求拦截器
+		return new MyInterceptor();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(myInterecptor());
+	}
+	
 }
